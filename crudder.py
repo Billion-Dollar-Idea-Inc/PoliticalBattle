fefb05e70ccf1121ce2eda3e900955b95c8b24c3
Derek Schuster <dschust1@binghamton.edu>
2015-10-30 19:00:20 -0400

    added database stuff

---
+++ crudder.py
@@ -0,0 +1,10 @@
+import sqlite3
+
+'''
+Manages the interaction between the program
+and the database. Appended from crudder.py
+in project github.com/Derrreks/CRUD
+'''
+class Crudder():
+	def __init__(self):
+		pass
