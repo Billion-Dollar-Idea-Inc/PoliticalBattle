bb78a913c264768bc9f561a7d22eae6cf293798a
Aiden Cullo <culloaiden3@gmail.com>
2015-10-30 19:37:48 -0400

    updated character

--- character.py
+++ character.py
@@ -1,5 +1,5 @@
 class Character(object):
-	def __init__(self, name):
+	def __init__(self, name):#variabel to intialize: health, party, attack, attack_description,
 		self.name = name
 		self.attacks = []
 
@@ -26,3 +26,37 @@ class Character(object):
 	def get_name(self):
 		'''returns name'''
 		return self.name
+	
+	def get_health(self):
+                 '''returns health'''
+                 return self.health
+
+	def add_health(self, boost):
+                 '''increases health'''
+                 self.health += boost
+
+	def lose_health(self, lose):
+                '''decreases health'''
+                self.health -= lose
+
+	def get_attack_description(self, attack):
+               '''retrieve description of attack'''
+                pass
+
+	def get_party(self):
+               '''returns party affiliation'''
+               return self.party
+
+	def get_random_attack(self):
+               '''return random attack'''
+                rand  = random.randint(0,len(attacks))
+		name = attack[rand]
+		power = dict[name]
+		toreturn = []
+		toreturn.append(name)
+		toreturn.append(power)
+		return toreturn 
+
+	def get_picture(self):
+               '''returns player picture'''
+               return self.image
