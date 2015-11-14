828adf53a44f793fe0a966ac3e2bb0647d4535a9
Derek <derek.schuster@yahoo.com>
2015-11-13 20:19:01 -0500

    fixed merge problems

--- main.py
+++ main.py
@@ -280,13 +280,10 @@ class GameScreen():
 	def set_descriptions(self, player):
 		c = crudder.Crudder()
 		self.descs = c.get_attack_descs(player.get_name())
-<<<<<<< HEAD
 	
 	def set_power(self, player):
 		c = crudder.Crudder()
 		self.powers = c.get_attack_powers(player.get_name())
-=======
->>>>>>> 3c4d12958e89dcee3dc5307f5f039317bf54fd3a
 
 	def get_attack(self, pos):
 		'''
@@ -331,7 +328,6 @@ class GameScreen():
 				return True
 		return False
 
-<<<<<<< HEAD
 	def attack(self, attackPos):
 		'''displays attack desc on the screen and decrements opp health'''
 		self.currentAttack = self.descs[attackPos];#sets attack selected description to speaking bubble
@@ -350,7 +346,7 @@ class GameScreen():
 		self.player.health -= self.oppAttack[1]
 		self.show_opp_attack = True
 		self.opp_turn = False
-=======
+		
 	#TODO
 	def attack(self, attack):
                 self.currentAttack = self.descs[attack];#sets attack selected description to speaking bubble
@@ -361,7 +357,6 @@ class GameScreen():
 		#decrement health from player
 		#wait
 		#then let player choose
->>>>>>> 3c4d12958e89dcee3dc5307f5f039317bf54fd3a
 
 	def render_text(self, screen, font, text, pos):
 		'''using the parameters, draws rectangle to the screen with the
@@ -397,10 +392,7 @@ class GameScreen():
 
 	def clear_attack_bubble(self):
 		self.show_attack = False
-<<<<<<< HEAD
 		self.show_opp_attack = False
-=======
->>>>>>> 3c4d12958e89dcee3dc5307f5f039317bf54fd3a
 
 	def get_game_screen(self, screen):
 		'''prints game screen to window'''
@@ -418,12 +410,9 @@ class GameScreen():
                         screen.blit(label, (self.attboxesx[x] + fontSize, self.attboxesy[x] + self.attboxesh/2 - fontSize/2))
 		if self.show_attack:
 			self.display_attack(screen, self.currentAttack , True)
-<<<<<<< HEAD
 		elif self.show_opp_attack:
 			self.display_attack(screen, self.currentOppAttack, False)
-=======
 		self.display_health(screen)
->>>>>>> 3c4d12958e89dcee3dc5307f5f039317bf54fd3a
 		return screen
 
 def main():
