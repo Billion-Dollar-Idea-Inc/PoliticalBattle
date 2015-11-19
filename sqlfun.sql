6ab7adaffbfb699800a62fc1ba680a0cef488131
Aiden Cullo <culloaiden3@gmail.com>
2015-11-18 19:15:14 -0500

    changed attack powers

---
+++ sqlfun.sql
@@ -0,0 +1,47 @@
+UPDATE attacks SET power = 45 WHERE attack = "Throw hairpiece";
+UPDATE attacks SET power = 35 WHERE attack = "elicit passport";
+UPDATE attacks SET power = 35 WHERE attack = "You're fired";
+
+UPDATE attacks SET power = 45 WHERE attack = "Happy to be here";
+UPDATE attacks SET power = 35 WHERE attack = "My dad died";
+
+UPDATE attacks SET power = 45 WHERE attack = "Violently wave arms";
+UPDATE attacks SET power = 35 WHERE attack = "Tired of Hillary's emails";
+UPDATE attacks SET power = 35 WHERE attack = "Not a socialist";
+
+UPDATE attacks SET power = 45 WHERE attack = "Woman card";
+UPDATE attacks SET power = 35 WHERE attack = "Ruin company";
+UPDATE attacks SET power = 35 WHERE attack = "Layoff employees";
+
+UPDATE attacks SET power = 55 WHERE attack = "Jeb fuel can't melt steel beams";
+UPDATE attacks SET power = 35 WHERE attack = "Question";
+UPDATE attacks SET power = 35 WHERE attack = "Stuff happens";
+
+UPDATE attacks SET power = 45 WHERE attack = "Jail makes you gay";
+UPDATE attacks SET power = 35 WHERE attack = "Obamacare is slavery";
+UPDATE attacks SET power = 35 WHERE attack = "I hate black people";
+
+UPDATE attacks SET power = 45 WHERE attack = "Could I get some time";
+UPDATE attacks SET power = 35 WHERE attack = "Vietnam";
+UPDATE attacks SET power = 35 WHERE attack = "Already president";
+
+UPDATE attacks SET power = 45 WHERE attack = "Terrible closing";
+UPDATE attacks SET power = 35 WHERE attack = "Get no votes";
+UPDATE attacks SET power = 35 WHERE attack = "Clean Energy";
+
+UPDATE attacks SET power = 45 WHERE attack = "Woman president";
+UPDATE attacks SET power = 35 WHERE attack = "Not losing again";
+UPDATE attacks SET power = 35 WHERE attack = "Take jab at Bernie";
+
+UPDATE attacks SET power = 45 WHERE attack = "Thanks Obama";
+UPDATE attacks SET power = 35 WHERE attack = "Oh Canada";
+UPDATE attacks SET power = 35 WHERE attack = "Religion Card";
+UPDATE attacks SET power = 35 WHERE attack = "Whacko bird";
+
+UPDATE attacks SET power = 45 WHERE attack = "Make Oval Office more #Chic";
+UPDATE attacks SET power = 35 WHERE attack = "Defend lying";
+UPDATE attacks SET power = 35 WHERE attack = "Compare 9/11 to Bengazi";
+
+UPDATE attacks SET power = 45 WHERE attack = "Close Bridge";
+UPDATE attacks SET power = 35 WHERE attack = "Say I'm fat";
+UPDATE attacks SET power = 35 WHERE attack = "Eat donut";
