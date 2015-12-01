35708e2c2ccac31859e6323e444fae64ec0b5547
Derek Schuster <dschust1@binghamton.edu>
2015-12-01 14:25:26 -0500

    closer

--- attckPowerFiller.py
+++ attckPowerFiller.py
@@ -2,14 +2,13 @@ import random
 import sqlite3
 import crudder
 
-#still needs to be tested, then ran
-
-
 crud = crudder.Crudder()
-conn = sqlite3.connect("data.db")
-c = self.conn.cursor()
+conn = sqlite3.connect("try.db")
+c = conn.cursor()
 
 names = crud.get_chars_in_party("DEM") + crud.get_chars_in_party("REP")
 for name in names:
-	num = random.randint(10, 30)
-	c.execute("UPDATE attacks SET power = {_num} WHERE name LIKE {_name}".format(_num = num, _name = name[0:5]))
+	attacks = crud.get_attacks(name)
+	for attack in attacks:
+		num = int(random.randint(10, 40))
+		c.execute("UPDATE attacks SET power = 30 WHERE attack = \"{rattack}\"".format(rattack = attack))
